0bc8e8e2e8b23549fdfaeebce5f0a48eec8b237e
semih-git <cemcan34355@gmail.com>
2021-01-13 21:46:57 +0100

    I created codingBookMainPageTest

--- src/test/java/org/cb/ta/Feature/CodingBookMainPage.feature
+++ src/test/java/org/cb/ta/Feature/CodingBookMainPage.feature
@@ -10,3 +10,9 @@ Feature: CodingBook Main Page
     When User checks if CodingBook LinkedIn icon is visible on the top right corner
     And User clicks CodingBook LinkedIn icon
     Then Test if user landed CodingBook LinkedIn page
+
+  Scenario: Test CodingBook Test Automation submenu under Courses menu
+    Given User lands to Courses menu on CodingBook web page
+    When User moves mouse to Test Automation sub menu under Courses menu
+    And User clicks Test Automation sub menu
+    Then Test if user landed Test Automation page
---
+++ src/test/java/org/cb/ta/TestSteps/CodingBookMainPageTest.java
@@ -0,0 +1,74 @@
+package org.cb.ta.TestSteps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CodingBookMainPageTest {
+
+        @Given("User opens CodingBook web page")
+        public void user_opens_coding_book_web_page() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("User checks if CodingBook logo is visible on the top left corner")
+        public void user_checks_if_coding_book_logo_is_visible_on_the_top_left_corner() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("User clicks CodingBook logo")
+        public void user_clicks_coding_book_logo() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Then("Test if user landed CodingBook homepage")
+        public void test_if_user_landed_coding_book_homepage() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("User checks if CodingBook LinkedIn icon is visible on the top right corner")
+        public void user_checks_if_coding_book_linked_in_icon_is_visible_on_the_top_right_corner() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("User clicks CodingBook LinkedIn icon")
+        public void user_clicks_coding_book_linked_in_icon() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Then("Test if user landed CodingBook LinkedIn page")
+        public void test_if_user_landed_coding_book_linked_in_page() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Given("User lands to Courses menu on CodingBook web page")
+        public void user_lands_to_courses_menu_on_coding_book_web_page() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("User moves mouse to Test Automation sub menu under Courses menu")
+        public void user_moves_mouse_to_test_automation_sub_menu_under_courses_menu() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("User clicks Test Automation sub menu")
+        public void user_clicks_test_automation_sub_menu() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Then("Test if user landed Test Automation page")
+        public void test_if_user_landed_test_automation_page() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+}
